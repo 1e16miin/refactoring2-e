287d741ab5c1c1987032edaa9d9c0ef87981c0b2
1e16miin <ddogi1513@naver.com>
2021-10-02 21:17:28 +0900

    Extract Function

--- src/main/ch1/statement.js
+++ src/main/ch1/statement.js
@@ -1,9 +1,7 @@
 const statement = (invoice, plays) => {
-  // let decodeInvoice = JSON.parse(invoice);
-  // const decodePlays = JSON.parse(plays);
+
   let totalAmount = 0;
   let volumeCredits = 0;
-  // let result = `billing history (customer name: ${invoices.customer})`;
   let result = {}
   result.customer = invoice.customer; 
   let historyList = [];
@@ -14,26 +12,9 @@ const statement = (invoice, plays) => {
   }).format;
   // console.log(invoice[0].customer);
   for (let perf of invoice.performance) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
+    const play = playFor(perf);
     let history = {}
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`Invalid Genre: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
@@ -48,8 +29,33 @@ const statement = (invoice, plays) => {
   result.historyList = historyList;
   result.totalAmount = format(totalAmount / 100);
   result.volumeCredits = volumeCredits;
-  console.log(result)
   return result;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance, play) {
+    let result = 0;
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`Invalid Genre: ${play.type}`);
+    }
+    return result;
+  }
 };
 
 
