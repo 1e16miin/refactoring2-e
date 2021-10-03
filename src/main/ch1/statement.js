a5cbe81c8ad37243981783d353112da68247f825
1e16miin <ddogi1513@naver.com>
2021-10-04 00:51:50 +0900

    Separate Calulation and Formatting

--- src/main/ch1/statement.js
+++ src/main/ch1/statement.js
@@ -1,37 +1,29 @@
+import createStatementData from './createStatementData.js';
+
 const statement = (invoice, plays) => {
+  return renderPlainText(createStatementData(invoice, plays));
+};
+
+const renderPlainText = (data) => {
   let result = {};
-  result.customer = invoice.customer;
+  result.customer = data.customer;
   let historyList = [];
 
-  for (let perf of invoice.performance) {
+  for (let perf of data.performance) {
     historyList.push(historyFor(perf));
   }
 
   result.historyList = historyList;
-  result.totalAmount = usd(totalAmount());
-  result.volumeCredits = totalVolumeCredits();
+  result.totalAmount = usd(data.totalAmount);
+  result.volumeCredits = data.totalVolumeCredits;
   return result;
 
-  function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performance) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoice.performance) {
-      result += volumeCreditFor(perf);
-    }
-    return result;
-  }
+  
 
   function historyFor(aPerformance) {
     let result = {};
-    result.playID = playFor(aPerformance).name;
-    result.amount = usd(amountFor(aPerformance));
+    result.playID = aPerformance.play.name;
+    result.amount = usd(aPerformance.amount);
     result.audience = aPerformance.audience;
     return result;
   }
@@ -41,42 +33,8 @@ const statement = (invoice, plays) => {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber/100);
-  }
-
-  function volumeCreditFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`Invalid Genre: ${playFor(aPerformance).type}`);
-    }
-    return result;
-  }
+    }).format(aNumber / 100);
+  }  
 };
 
 export default statement;
